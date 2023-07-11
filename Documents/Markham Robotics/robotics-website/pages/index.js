f1f37ded5f3e55dc0bd6de7a6aaa38ce64f98119
Derrick <derrickr318@yahoo.com>
2023-07-10 22:52:06 -0400

    Added styling to the collapse button and column 3 animations

--- Documents/Markham Robotics/robotics-website/pages/index.js
+++ Documents/Markham Robotics/robotics-website/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Metrics from '../components/metrics'
 import { Fragment, useState } from 'react'
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 
 
 export default function Home() {
@@ -17,7 +17,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.column}>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />  
+      <div className={styles.column}>
           <h2 className={styles.titles}><u>MORE METRICS</u></h2>
           <p>This is the first column.</p>
           <div className={styles.metricContainer}>
@@ -43,6 +45,19 @@ export default function Home() {
           </select>
           <div className={styles.split}>
             <div className={styles.subColumn1}>
+            {
+              role == "editor" &&
+              <Fragment>
+              {
+              collapsible == true &&
+              <motion.button onClick={() => {setCollapsible(!collapsible)}} className={styles.collapseClosed} animate={{x: 0}} initial={{x:100}} transition={{ ease: "easeInOut" }}>
+              <span class="material-symbols-outlined">
+              arrow_back_ios
+              </span>
+              </motion.button>
+          }
+              </Fragment>
+            }
               <h2 className={styles.titles}><u>ANALYTICS</u></h2>
               <p>This is the first sub-column of the second column.</p>
               {(metricStats.length == 0) &&
@@ -84,37 +99,47 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <AnimatePresence>
         {
           role == "editor" &&
           <Fragment>
-            <button onClick={() => {setCollapsible(!collapsible)}}>
-              Collapse
-              </button>
+            {
+              collapsible == false &&
+              <motion.div className={styles.column3} animate={{x: 0}} initial={{x:"50%"}} exit={{x:"100%"}}>
+              <motion.button onClick={() => {setCollapsible(!collapsible)}} className={styles.collapse}  animate={{scale: 1}}
+              initial={{scale:0}}>
+              <span class="material-symbols-outlined">
+              arrow_forward_ios
+              </span>
+                </motion.button>
 
-                {
-                  collapsible == false &&
-                  <div className={styles.column3}>
-                  <h2 className={styles.titles}><u>EDIT ANALYTICS</u></h2>
-                  <p>This is the third column.</p>
-                  <ul className={styles.table}>
-                  {metricStats.map((item, idx) => {
-                    return(
-                        <motion.li 
-                        animate={{scale: 1}}
-                        initial={{scale:0.95}}
-                        className={styles.indieStat} key={idx}>
-                          <div className={styles.tableElement}>
-                            {item}
-                          </div>
-                        </motion.li>
-                    )
-                  })}
-                  </ul>
                   
-                </div>
-                }
+                    <div>
+                    <h2 className={styles.titles}><u>EDIT ANALYTICS</u></h2>
+                    <p>This is the third column.</p>
+                    <ul className={styles.table}>
+                    {metricStats.map((item, idx) => {
+                      return(
+                          <motion.li 
+                          animate={{scale: 1}}
+                          initial={{scale:0.95}}
+                          className={styles.indieStat} key={idx}>
+                            <div className={styles.tableElement}>
+                              {item}
+                            </div>
+                          </motion.li>
+                      )
+                    })}
+                    </ul>
+                    
+                  </div>
+                  
+            </motion.div>}
           </Fragment>
+          
         }
+        </AnimatePresence>
+        
       </main>
     </div>
   )
